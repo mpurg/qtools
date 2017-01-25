98e72ab2e0b408dddf8e410c0b374ff81aec06c8
Miha Purg <miha.purg@gmail.com>
2017-01-25 20:49:53 +0100

    Added ff14SB to Q conversion test

---
+++ tests/Qpyl/core/qtopology_test.py
@@ -0,0 +1,63 @@
+from Qpyl.core.qparameter import QPrm
+from Qpyl.core.qlibrary import QLib
+from Qpyl.core.qstructure import QStruct
+from Qpyl.core.qtopology import QTopology
+
+
+def test_ff14sb_conversion():
+    # Amber14FF to Qamber14
+    #
+    # Convert Amber14 lib (+prepin for impropers) and parm+frcmod to Q lib/prm
+    # Load all_amino_acids.pdb and all_amino_acids.mol2 and build topology
+    # separately.
+    #
+    qal = QLib("amber")
+    qap = QPrm("amber", ignore_errors=True) # duplicates
+    qal.read_amber_lib("data/ff-amber14/amber12_mod.lib")
+    qal.read_amber_lib("data/ff-amber14/arn.lib")
+    qal.read_prepin_impropers("data/ff-amber14/prep/amino12.in")
+    qal.read_prepin_impropers("data/ff-amber14/arn.prepi")
+    qap.read_amber_parm("data/ff-amber14/parm/parm10.dat")
+    qap.read_amber_frcmod("data/ff-amber14/parm/frcmod.ff14SB")
+
+    # add options to parameters
+    for line in """name		Q-Amber14SB
+type		AMBER
+vdw_rule    arithmetic !vdW combination rule (geometric or arithmetic)
+scale_14	0.8333 ! electrostatic 1-4 scaling factor
+switch_atoms	off
+improper_potential	periodic
+improper_definition explicit""".splitlines():
+        lf = line.split()
+        qap.options[lf[0]] = " ".join(lf[1:])
+
+    # remove head from ACE and tail from NME
+    cons = qal.residue_dict["ACE"].connections
+    cons = [con for con in cons if "head" not in con]
+    qal.residue_dict["ACE"].connections = cons
+
+    cons = qal.residue_dict["NME"].connections
+    cons = [con for con in cons if "tail" not in con]
+    qal.residue_dict["NME"].connections = cons
+
+
+    qas1 = QStruct("data/all_amino_acids.pdb", "pdb", ignore_errors=True)
+    qat = QTopology(qal, qap, qas1)
+    q_tors = sum([len(list(tor.prm.get_prms())) for tor in qat.torsions])
+
+    assert len(qat.bonds) == 464
+    assert len(qat.angles) == 829
+    assert len(qat.torsions) == 1221
+    assert q_tors == 1950
+    assert len(qat.impropers) == 102
+
+    be = sum([bond.calc()[0] for bond in qat.bonds])
+    ae = sum([ang.calc()[0] for ang in qat.angles])
+    te = sum([tor.calc()[0] for tor in qat.torsions])
+    ie = sum([imp.calc()[0] for imp in qat.impropers])
+
+    assert abs(be - 181.2572830) < 1e-7
+    assert abs(ae - 212.8539304) < 1e-7
+    assert abs(te - 417.2919960) < 1e-7
+    assert abs(ie -  23.0370760) < 1e-7
+
