88d74c8cbe97802a4ccbcff9bdc91771e0758471
Miha Purg <miha.purg@gmail.com>
2018-04-24 12:09:17 +0200

    Version 0.6.0
    Changes from 0.5.12
    - QAnalyseDyns refactored
    - Group contributions include dE1, dE2 and REORG output
    - Tests for q_analysedyns/QAnalyseDyns/QDynOutput
    - Tests for q_mapper/q_automapper/QMapper using precompiled Q6
    - Tests for q_calc/QGroupContrib using precompiled Q6
    - Better support for Q6
    - Minor bugfixes and cosmetic changes

--- qscripts-cli/q_genfeps.py
+++ qscripts-cli/q_genfeps.py
@@ -3,7 +3,7 @@
 #
 # MIT License
 # 
-# Copyright (c) 2017  Miha Purg <miha.purg@gmail.com>
+# Copyright (c) 2018  Miha Purg <miha.purg@gmail.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
