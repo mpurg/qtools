368b0b064d916eb7befcf36944aa137f420f9467
Miha Purg <miha.purg@gmail.com>
2018-09-05 12:26:18 +0200

    Added reading of gzipped qdyn outputs and fixed multiplicity issues.
    Q, unlike Amber, does not require negative multiplicities/periodicities
    for grouping torsional parameters (it doesn't group them).
    It also evaluates the negative sign in the energy expression, thus two
    parameters with opposite multiplicity and phase shift not equal to 0 or 180
    degrees, yield different results.
    Example:
    fc=3  mult=2  psi0=120
    fc=3  mult=-2  psi0=120
    
    All parameters from Amber formats are now read as having positive
    multiplicities.
    All parameters with 0 and 180 phase shifts are now read and
    written as positive to prevent conflicts with parameter sets
    containing negative values.
    
    Updated the tests.

--- packages/Qpyl/common.py
+++ packages/Qpyl/common.py
@@ -32,6 +32,7 @@ import sys
 import os
 import shutil
 import logging
+import gzip
 
 logger = logging.getLogger(__name__)
 
@@ -343,4 +344,47 @@ class DataContainer(object):
                 else:
                     outs += " {val:{width}} ".format(val=str(val), width=width)
         return outs
+
+
+# Shamelessly borrowed from
+# http://www.genomearchitecture.com/2014/01/how-to-gunzip-on-the-fly-with-python
+class gzopen(object):
+    """Generic opener that decompresses gzipped files
+    if needed. Encapsulates an open file or a GzipFile.
+    Use the same way you would use 'open()'.
+    """
+    def __init__(self, fname):
+        f = open(fname)
+        # Read magic number (the first 2 bytes) and rewind.
+        magic_number = f.read(2)
+        f.seek(0)
+        # Encapsulated 'self.f' is a file or a GzipFile.
+        if magic_number == '\x1f\x8b':
+            self.f = gzip.GzipFile(fileobj=f)
+        else:
+            self.f = f
+
+    # Define '__enter__' and '__exit__' to use in
+    # 'with' blocks. Always close the file and the
+    # GzipFile if applicable.
+    def __enter__(self):
+        return self
+    def __exit__(self, type, value, traceback):
+        try:
+            self.f.fileobj.close()
+        except AttributeError:
+            pass
+        finally:
+            self.f.close()
+
+    # Reproduce the interface of an open file
+    # by encapsulation.
+    def __getattr__(self, name):
+        return getattr(self.f, name)
+    def __iter__(self):
+        return iter(self.f)
+    def next(self):
+        return next(self.f)
+
+
 
